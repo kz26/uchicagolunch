0207404dc6b2677105190b06309214f75451a7f5
Kevin Zhang <whitehat2k9@gmail.com>
2011-11-27 15:33:08 -0600

    favicon and icon

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
-from django.views.generic.simple import direct_to_template
+from django.views.generic.simple import *
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -9,6 +9,7 @@ urlpatterns = patterns('',
     url(r'^$', 'site_main.views.home'),
     url(r'^faq/$', direct_to_template, {'template': 'faq.html'}, name="faq"),
     url(r'^tos/$', direct_to_template, {'template': 'terms.html'}, name="terms"),
+    url(r'^favicon.ico/$', redirect_to, {'url': 'http://static.uchicagolunch.com/favicon.ico'}),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
